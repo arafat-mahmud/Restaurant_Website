00d1bb1ccedf64cee97e5806406180fde3fffd6d
Arafat Mahmud <104220863+arafat-mahmud@users.noreply.github.com>
2023-11-08 22:02:32 +0600

    ok

--- Project/First_App/admin.py
+++ Project/First_App/admin.py
@@ -5,8 +5,6 @@ from First_App.models import Contact
 
 class AdminContact(admin.ModelAdmin):
     list_display = ['name', 'email','subject', 'message']
-
-
 admin.site.register(Contact, AdminContact)
 
 
